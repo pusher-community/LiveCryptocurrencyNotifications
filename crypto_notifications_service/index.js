096739eb45089b68b1aaac21ed3c2ff77fe98945
Zan Markan <zan.markan@gmail.com>
2017-08-01 14:24:50 +0100

    Javascripted the server component and cleaned up the deps

---
+++ crypto_notifications_service/index.js
@@ -0,0 +1,56 @@
+const Pusher = require('pusher');
+const request = require('sync-request');
+
+var pusher = new Pusher({
+    appId: '354052',
+    key: 'a446158132cfaa3ed747',
+    secret: '6f6ddfd0127e04d23cbb',
+    encrypted: true
+});
+
+let graph_url = 'https://bitcoincharts.com/charts/chart.png?width=940&m=bitstampUSD&SubmitButton=Draw&r=2&i=15-min&c=0&s=&e=&Prev=&Next=&t=M&b=&a1=&m1=10&a2=&m2=25&x=0&i1=&i2=&i3=&i4=&v=1&cv=1&ps=0&l=0&p=0&'
+let graph_url_minute = 'https://bitcoincharts.com/charts/chart.png?width=940&m=bitstampUSD&SubmitButton=Draw&r=1&i=1-min&c=0&s=&e=&Prev=&Next=&t=M&b=&a1=&m1=10&a2=&m2=25&x=0&i1=&i2=&i3=&i4=&v=1&cv=1&ps=0&l=0&p=0&';
+
+var cron = require('node-cron');
+
+let counter = 0;
+
+const updatePrice = () => {
+     let btcprice = JSON.parse(request('GET', 'https://www.bitstamp.net/api/v2/ticker_hour/btcusd/').getBody('utf8'));
+    
+
+    let currentPrice = btcprice.last;
+    let openPrice = btcprice.open;
+    let currencyPair = "BTCUSD";
+
+    counter += 1;
+
+    pusher.notify(['bitcoin'], {
+        fcm: {
+            data: {
+                graphUrl: graph_url_minute,
+                currentPrice: currentPrice,
+                openPrice: openPrice,
+                currencyPair: currencyPair,
+                counter: counter
+            }
+        }
+    });
+
+    console.log(
+        {
+            graphUrl: graph_url_minute,
+            currentPrice: currentPrice,
+            openPrice: openPrice,
+            currencyPair: currencyPair,
+            counter: counter
+        });
+}
+
+
+var task = cron.schedule('*/15 * * * *', () => {
+    updatePrice();
+});
+
+updatePrice();
+
